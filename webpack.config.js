f2f3007cd6cc4100f0d493a6bbcb2f14403d2280
mrdck <mrdck@icloud.com>
2017-08-25 01:47:16 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+module.exports = {
+    entry: './src/index.js',
+      output: {
+    path: __dirname,
+    filename: './dist/app.bundle.js'
+  },
+    module:{
+        rules: [
+            {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        },
+         {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+        ]
+    },
+    devServer:{
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: process.env.PORT
+    },
+    plugins: [new HtmlWebpackPlugin({
+         template: './src/index.html'
+    }),
+     new ExtractTextPlugin('style.css')
+    ]
+}
